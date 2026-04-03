0998be14662e5c2a95a1798282a20a1725356dc3
Kundan Kumar Singh <kundankumarsingh2005@gmail.com>
2026-04-03 14:27:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
@@ -19,5 +19,47 @@ class Solution {
         // code here
         int n=val.size();
         return fun(wt,val,W,n);
+        // vector<vector<int>>t(n+1,vector<int>(W+1));
+        
+        //base condition first row and second row =0
+        // for(int i=0; i<t.size(); i++){
+        //     for(int j=0; j<t[0].size();j++){
+        //         if(i==0 || j==0){
+        //             t[i][j]=0;
+        //         }
+        //     }
+        // }
+        
+        // for(int i=1; i<t.size(); i++){
+        //     for(int j=1; j<t[0].size(); j++){
+        //         if(wt[i-1]<=j){
+        //             t[i][j]=max(val[i-1]+t[i-1][j-wt[i-1]],t[i-1][j]);
+                    
+        //         }else{
+        //             t[i][j]=t[i-1][j];
+        //         }
+        //     }
+        // }
+        // return t[n][W];
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
 };
