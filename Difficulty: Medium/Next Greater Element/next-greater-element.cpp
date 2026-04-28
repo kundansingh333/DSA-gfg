44153f21842db2b736be41be5efaf829ac037b65
Kundan Kumar Singh <kundankumarsingh2005@gmail.com>
2026-04-28 14:47:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Next Greater Element/next-greater-element.cpp
+++ Difficulty: Medium/Next Greater Element/next-greater-element.cpp
@@ -2,27 +2,42 @@ class Solution {
   public:
     vector<int> nextLargerElement(vector<int>& arr) {
         // code here
-        stack<int> st;
+        // int n=arr.size();
+        // vector<int>ans(n,-1);
+        // for(int i=0; i<n-1; i++){
+        //     for(int j=i+1; j<n; j++){
+        //         if(arr[j]>arr[i]){
+        //             ans[i]=arr[j];
+        //             break;
+        //         }
+        //     }
+        // }
+        // return ans;
+        
+        
         vector<int>ans;
-        for(int i=arr.size()-1; i>=0; i--){
-            if(st.empty()){
+        stack<int>st;
+        
+        int n=arr.size();
+        for(int i=n-1; i>=0; i--){
+            if(st.size()==0){
                 ans.push_back(-1);
-            }else if(st.top()>arr[i]){
+            }else if(st.size()>0 && st.top()>arr[i]){
                 ans.push_back(st.top());
-            }else if(st.top()<=arr[i]){
-                while(!st.empty() && st.top()<=arr[i]){
+            }else if(st.size()>0 && st.top()<=arr[i]){
+                while(st.size()>0 && st.top()<=arr[i]){
                     st.pop();
                 }
-                if(st.empty()){
+                if(st.size()==0){
                     ans.push_back(-1);
                 }else{
                     ans.push_back(st.top());
                 }
-                
             }
             st.push(arr[i]);
         }
         reverse(ans.begin(),ans.end());
         return ans;
+        
     }
 };
