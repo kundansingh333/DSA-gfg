69d0ec07e974e84e34337dbc37bef95ac0d6975f
Kundan Kumar Singh <kundankumarsingh2005@gmail.com>
2026-08-24 22:47:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
@@ -17,6 +17,34 @@ class Solution {
         current_path[src]=0;
         return false;
     }
+    
+    bool topoSort(vector<vector<int>>&adj,int V){
+        vector<int>indegree(V,0);
+        for(int i=0; i<V; i++){
+            for(auto &x:adj[i]){
+                indegree[x]+=1;
+            }
+        }
+        queue<int>q;
+        for(int i=0; i<indegree.size(); i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+        vector<int>ans;
+        while(!q.empty()){
+            int f=q.front();
+            q.pop();
+            ans.push_back(f);
+            for(auto &nbr:adj[f]){
+                indegree[nbr]--;
+                if(indegree[nbr]==0){
+                    q.push(nbr);
+                }
+            }
+        }
+        return ans.size()!=V;
+    }
     bool isCyclic(int V, vector<vector<int>> &edges) {
         // code here
         vector<vector<int>>adj(V);
@@ -25,14 +53,23 @@ class Solution {
             int b=edge[1];
             adj[a].push_back(b);
         }
-        vector<bool>visited(V,false);
-        vector<int>current_path(V,0);
-        for(int i=0; i<V; i++){
-            if(!visited[i]){
-                bool ans=fun(i,adj,visited,current_path);
-                if(ans) return true;
-            }
-        }
-        return false;
+        // vector<bool>visited(V,false);
+        // vector<int>current_path(V,0);
+        // for(int i=0; i<V; i++){
+        //     if(!visited[i]){
+        //         bool ans=fun(i,adj,visited,current_path);
+        //         if(ans) return true;
+        //     }
+        // }
+        // return false;
+        
+        
+        
+        //topologica sort
+        bool ans=topoSort(adj,V);
+        return ans;
+        
+        
+        
     }
 };
